d840d82172a3d58be165eda70e8ec4d83aeaac69
dfinn2 <dfinn2@gmail.com>
2025-02-26 07:54:31 -0800

    feat: add Navbar and Footer components to product pages and update tsconfig paths

---
+++ src/app/products/nnn/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from 'src/components/global/Navbar';
+import Footer from 'src/components/global/Footer';
+
+export default function Example() {
+  
+  return (
+    <div className="bg-white">
+      <Navbar />
+
+      <main><h1>NNN Page</h1></main>
+      <Footer />
+    </div>
+  );        
+}
