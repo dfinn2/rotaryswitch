f6b240e74be499fcccd1307c066abaf84108b249
dfinn2 <dfinn2@gmail.com>
2025-02-26 07:46:46 -0800

    refactor: remove unused navigation state from page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 'use client'
 
-import { useState } from 'react'
 import Navbar from '../components/global/Navbar'
 import Footer from  '../components/global/Footer'
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
 
 export default function Example() {
   
